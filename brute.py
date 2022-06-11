e07f01737eed82fb92bdda19288c010c8b03fc75
dorokdok <90540407+dorokdok@users.noreply.github.com>
2022-06-11 12:15:14 +0700

    A

--- brute.py
+++ brute.py
@@ -13,17 +13,16 @@ def brute(a, n):
         i -= 1
     return dmp
 
-def LIS(a, N):
+def LIS(a):
     dmp = 1
-    for i in range(N):
+    for i in range(len(a)):
         dmp = max(dmp, brute(a,i))
     return dmp
 
 def main():
     global perbandingan
     list = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-    dmp = LIS(list, len(list))
-    print("Maksimum LIS:", dmp)
+    print("Maksimum LIS:", LIS(list))
     print("Total Perbandingan:",perbandingan)
 
 
--- dynamic.py
+++ dynamic.py
@@ -1,23 +1,19 @@
 perbandingan = 0
 def LISwithDP(a):
     global perbandingan
-    dmp = []
-    for i in range (len(a)):
-        dmp.append(0)
+    dmp = [0] * len(a)
     dmp[0] = 1
     for i in range(1,len(a)):
-        for j in range(i-1):
+        for j in range(i):
             if a[j] < a[i]:
                 dmp[i] = max(dmp[i], dmp[j] + 1)
                 perbandingan += 1
-    Max = max(dmp)
-    return Max
+    return max(dmp)
 
 def main():
     global perbandingan
     list = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-    dmp = LISwithDP(list)
-    print("Maksimum LIS:", dmp)
+    print("Maksimum LIS:", LISwithDP(list))
     print("Total Perbandingan:",perbandingan)
 
 main()
